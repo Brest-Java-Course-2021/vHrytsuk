9bbd1385c1596ed62d50a57f40e0f4d8afa9ffed
grits <grits.0123@gmail.com>
2021-02-05 19:13:36 +0300

    We added function of input by user and check this inputed data for correctness.

--- price-calc/src/main/java/com/epambrest/Main.java
+++ price-calc/src/main/java/com/epambrest/Main.java
@@ -1,5 +1,7 @@
 package com.epambrest;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Distance
@@ -8,13 +10,54 @@ public class Main {
         // Price per kg
         // Result = d * pr1 + w * pr2
 
-        double distance = -10;
-        double price1 = 20.05;
-        double weight = 30;
-        double price2 = 30.5;
+        Double[] enteredValues = new Double[4];
+        Scanner scanner = new Scanner(System.in);
+        String inputValue;
 
-        double result = distance * price1 + weight * price2;
+        //File(s) for prices
+        //Change calc function
+        //Change user dialog
 
-        System.out.println("Result: " + result);
+        int i = 0;
+        do {
+            if (i == 0) {
+                System.out.println("Please, enter distance:");
+            } else if (i == 1) {
+                System.out.println("Please, enter price per km:");
+            } else if (i == 2) {
+                System.out.println("Please, enter weight:");
+            } else if (i == 3) {
+                System.out.println("Please, enter price per kg:");
+            }
+
+            inputValue = scanner.next();
+
+            if (inputValue.equalsIgnoreCase("Q")) {
+                break;
+            } else if (isCorrectDoubleValue(inputValue)) {
+                enteredValues[i] = Double.parseDouble(inputValue);
+                i++;
+            } else {
+                System.out.println("Incorrect value: " + inputValue);
+            }
+
+            if (i == 4) {
+                Double result = enteredValues[0] * enteredValues[1] + enteredValues[2] * enteredValues[3];
+                System.out.println("Result : " + result);
+            }
+
+        } while (i < 4);
+    }
+
+    private static boolean isCorrectDoubleValue(String value) {
+        boolean checkResult;
+        try {
+            double enteredValue = Double.parseDouble(value);
+            checkResult = enteredValue >= 0;
+        } catch (NumberFormatException ex) {
+            checkResult = false;
+        }
+
+        return checkResult;
     }
 }
